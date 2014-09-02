b1649c6289412461a8dd23aef8e4843b752af19c
falkzach <falkzach@gmail.com>
2014-09-02 13:49:56 -0600

    initial model

--- recipes/main/admin.py
+++ recipes/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from recipes.main.models import Recipe, Ingredient, Category
 
-# Register your models here.
+
+admin.autodiscover()
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
+admin.site.register(Category)
