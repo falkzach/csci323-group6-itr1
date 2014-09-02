e2efb43c9d82fc1e4cd5f3ec325356aeaa43f71a
falkzach <falkzach@gmail.com>
2014-09-02 13:49:56 -0600

    initial model

--- recipes/main/models.py
+++ recipes/main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+
+    name = models.CharField(max_length=255,)
+
+    description = models.CharField(max_length=255,)
+
+    rating = models.IntegerField()
+
+    directions_id = models.IntegerField
+
+    #further investigate relationships
+    ingredients = models.ManyToManyField(Ingredient)
+
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+
+    name = models.CharField(max_length=255,)
+
+    unit_of_measure = models.CharField(max_length=255,)
+
+    value_of_measure = models.IntegerField()
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255,)
+
+    description = models.CharField(max_length=255,)
+
