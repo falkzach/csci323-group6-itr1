edc1df1a89ae5eca7de37d372d1ca69b8df5c8d5
Benjamin Campbell <benji@benjica.com>
2014-09-02 22:29:32 -0600

    Moved to root url

--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'recipes.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^/', include(urls)),
+    url(r'^$', include(urls)),
+    url(r'^', include(urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
