9ade3328ae349fa05b5b44d38f244f8e1abdfec9
falkzach <falkzach@gmail.com>
2014-09-02 22:23:10 -0600

    removed unneeded import

--- recipes/main/admin.py
+++ recipes/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import Recipe, Ingredient, Category, Direction
+from main.models import Recipe, Ingredient, Category
 
 
 admin.site.register(Recipe)
