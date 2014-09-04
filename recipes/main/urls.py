f46004231caadf4fad7f6b9121a02cd7ffc20052
falkzach <falkzach@gmail.com>
2014-09-04 13:34:03 -0600

    working links, Category controller is broken

--- recipes/main/urls.py
+++ recipes/main/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from main.views import Recipes
+from main.views import Recipes, Category
 
 urlpatterns = [
         url(r'^$', Recipes.as_view()),
-        url(r'^recipes$', Recipes.as_view()),
-        url(r'^recipes/(?P<recipe_id>[0-9]+)$', Recipes.as_view()),
+        url(r'^recipes$', Recipes.as_view(), name="recipes"),
+        url(r'^recipes/(?P<recipe_id>[0-9]+)$', Recipes.as_view(), name="recipe"),
+        url(r'^categories$', Category.as_view(), name="categories"),
+        url(r'^categories/(?P<category_id>[0-9]+)$', Category.as_view(), name="category"),
 
 ]
--- recipes/main/views.py
+++ recipes/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import re
+from django.http import HttpResponseNotFound
+
 
 from django.views.generic.base import View
 
@@ -12,9 +13,23 @@ class Recipes(View):
             recipes = Recipe.objects.all()
             return render(request, 'recipes.html', {'recipes':recipes})
         else:
-            recipe = Recipe.objects.get(id=recipe_id)
-            ingredients = Ingredient.objects.filter(recipe__id=recipe_id)
+            recipe = Recipe.objects.get(id=recipe_id).order_by('name')
+            ingredients = Ingredient.objects.filter(recipe__id=recipe_id).order_by('name')
             directions = Direction.objects.filter(recipe__id=recipe_id).order_by('step')
             return render(request, 'recipe.html', {'recipe':recipe,
                                                    'ingredients':ingredients,
                                                    'directions':directions})
+
+class Category(View):
+    def get(self, request, category_id=None):
+        if category_id is None:
+            categories = Category.objects.all()
+            return render(request, 'categories.html', {'categories':categories})
+        else:
+            try:
+                category = Category.objects.get(id=category_id).order_by('name')
+                recipes = Recipe.objects.get(category__id=category_id).order_by('name')
+                return render(request, 'category.html', {'category':category,
+                                                         'recipes':recipes})
+            except:
+                return HttpResponseNotFound('<h1>Page not found</h1>')
