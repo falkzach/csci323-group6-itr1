1ddef2fc7ca9bf45d2e3372ca011aafc38a55c9d
falkzach <falkzach@gmail.com>
2014-09-03 23:48:28 -0600

    working recipes and recipe pages
    have not wired up links on recipe's list
    http://127.0.0.1:8000/recipes/1

--- recipes/main/urls.py
+++ recipes/main/urls.py
@@ -4,5 +4,7 @@ from main.views import Recipes
 
 urlpatterns = [
         url(r'^$', Recipes.as_view()),
+        url(r'^recipes$', Recipes.as_view()),
+        url(r'^recipes/(?P<recipe_id>[0-9]+)$', Recipes.as_view()),
 
 ]
--- recipes/main/views.py
+++ recipes/main/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re
 
 from django.views.generic.base import View
 
-from main.models import Recipe, Ingredient, Category
+from main.models import Recipe, Ingredient, Category, Direction
 
 class Recipes(View):
-    def get(self, request, id=None):
-        recipes = Recipe.objects.all()
-        return render(request, 'recipes.html', {'recipes':recipes})
+    def get(self, request, recipe_id=None):
+        if recipe_id is None:
+            recipes = Recipe.objects.all()
+            return render(request, 'recipes.html', {'recipes':recipes})
+        else:
+            recipe = Recipe.objects.get(id=recipe_id)
+            ingredients = Ingredient.objects.filter(recipe__id=recipe_id)
+            directions = Direction.objects.filter(recipe__id=recipe_id).order_by('step')
+            return render(request, 'recipe.html', {'recipe':recipe,
+                                                   'ingredients':ingredients,
+                                                   'directions':directions})
