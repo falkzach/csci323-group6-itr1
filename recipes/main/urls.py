93243fdc4e5fbf6ace6ad3dd1100237f5c66155a
falkzach <falkzach@gmail.com>
2014-09-03 22:54:44 -0600

    i can has working frontend!!!

--- recipes/main/urls.py
+++ recipes/main/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import url
 
-from main.views import RecipeCollectionView, RecipeResourceView, Recipes
+from main.views import Recipes
 
 urlpatterns = [
-        url(r'^$', RecipeCollectionView.as_view()),
-        url(r'^(yes)$', Recipes.as_view()),
-        url(r'^(?P<id>[0-9]+)$', RecipeResourceView.as_view(), name="recipes_view"),
-        url(r'^(?P<sort>alphabetical)$', RecipeCollectionView.as_view(), name="alphabetical_list"),
-        url(r'^(?P<sort>alphabetical)/(?P<pivot>[a-zA-Z])$', RecipeCollectionView.as_view()),
-        url(r'^(?P<sort>ingredient|category)$', RecipeCollectionView.as_view(), name="ingredient_category_sort"),
-        url(r'^(?P<sort>ingredient|category)/(?P<pivot>[0-9]+)$', RecipeCollectionView.as_view()),
+        url(r'^$', Recipes.as_view()),
+
 ]
--- recipes/main/views.py
+++ recipes/main/views.py
@@ -5,56 +5,7 @@ from django.views.generic.base import View
 
 from main.models import Recipe, Ingredient, Category
 
-# Create your views here.
-
-class RecipeCollectionView(View):
-
-    def get(self, request, sort=None, pivot=None):
-        ''' The method is responsible for handling collection get requests'''
-        
-        resource_dict = {}
-        
-        if sort == 'ingredient':
-            if pivot:
-                bucket = Ingredient.objects.get(pk=pivot).recipe_set.all()
-            else:
-                bucket = Ingredient.objects.all()
-            
-        elif sort == 'category':
-            if pivot:
-                bucket = Ingredient.objects.get(pk=pivot).recipe_set.all()
-            else:
-                bucket = Category.objects.all()
-        else:
-            # If not sort is specified just get all recipes
-            bucket = Recipe.objects.all()
-
-        for drop in bucket:
-            
-            key = drop.name.upper()[0]
-
-            if key not in resource_dict:
-                resource_dict[key] = list()
-
-            resource_dict[key].append(drop)
-
-        football = {'resource_collection': resource_dict, 'title': sort}
-        print(football)
-        return render(request, 'index.html', football)
-
-    def post(self, request):
-        '''Handles posts of new recipes from a form'''
-
-        return HttpResponse('POST /')
-
-class RecipeResourceView(View):
-    def get(self, request, id=None):
-        return HttpResponse('Would return a single recipe object with id %s' % (id))
-
 class Recipes(View):
     def get(self, request, id=None):
         recipes = Recipe.objects.all()
-        bob = "hi"
-        print(bob)
-        print (recipes)
-        return render(request, 'recipes.html', recipes)
+        return render(request, 'recipes.html', {'recipes':recipes})
