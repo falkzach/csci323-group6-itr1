d4d1b426ccc33cb4a26231473f4cf28952df131b
falkzach <falkzach@gmail.com>
2014-09-04 19:23:41 -0600

    added ingredients pages

--- recipes/main/urls.py
+++ recipes/main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from main.views import Recipes, Categories
+from main.views import Recipes, Categories, Ingredients
 
 urlpatterns = [
         url(r'^$', Recipes.as_view()),
@@ -8,5 +8,8 @@ urlpatterns = [
         url(r'^recipes/(?P<recipe_id>[0-9]+)$', Recipes.as_view(), name="recipe"),
         url(r'^categories$', Categories.as_view(), name="categories"),
         url(r'^categories/(?P<category_id>[0-9]+)$', Categories.as_view(), name="category"),
+        url(r'^ingredients$', Ingredients.as_view(), name="ingredients"),
+        url(r'^ingredients/(?P<ingredient_id>[0-9]+)$', Ingredients.as_view(), name="ingredient"),
+
 
 ]
--- recipes/main/views.py
+++ recipes/main/views.py
@@ -10,25 +10,36 @@ from main.models import Recipe, Ingredient, Category, Direction
 class Recipes(View):
     def get(self, request, recipe_id=None):
         if recipe_id is None:
-            recipes = Recipe.objects.all()
+            recipes = Recipe.objects.all().order_by('name')
             return render(request, 'recipes.html', {'recipes':recipes})
         else:
             recipe = Recipe.objects.get(id=recipe_id)
             ingredients = Ingredient.objects.filter(recipe__id=recipe_id)
             directions = Direction.objects.filter(recipe__id=recipe_id).order_by('step')
             categories = Category.objects.all().filter(recipes__id=recipe_id)
-            return render(request, 'recipe.html', {'recipe' : recipe,
-                                                   'ingredients' : ingredients,
-                                                   'directions' : directions,
-                                                   'categories' : categories})
+            return render(request, 'recipe.html', {'recipe': recipe,
+                                                   'ingredients': ingredients,
+                                                   'directions': directions,
+                                                   'categories': categories})
 
 class Categories(View):
     def get(self, request, category_id=None):
         if category_id is None:
             categories = Category.objects.all()
-            return render(request, 'categories.html', {'categories':categories})
+            return render(request, 'categories.html', {'categories': categories})
         else:
             category = Category.objects.get(id=category_id)
             recipes = Recipe.objects.all().filter(category__id=category_id)
-            return render(request, 'category.html', {'category':category,
-                                                     'recipes':recipes})
+            return render(request, 'category.html', {'category': category,
+                                                     'recipes': recipes})
+
+class Ingredients(View):
+    def get(self, request, ingredient_id=None):
+        if ingredient_id is None:
+            ingredients = Ingredient.objects.all()
+            return render(request, 'ingredients.html', {'ingredients' : ingredients})
+        else:
+            ingredient = Ingredient.objects.get(id=ingredient_id)
+            recipes = Recipe.objects.all().filter(ingredients__id=ingredient_id)
+            return render(request, 'ingredient.html', {'ingredient': ingredient,
+                                                       'recipes': recipes})
